b9ec61d4093b49472b2b67cff95ebeef5f6f325e
codeNoobNann <carrywu19962904@gmail.com>
2017-03-23 11:08:42 +0800

    Add files via upload

--- tserver01.js
+++ tserver01.js
@@ -3,7 +3,7 @@
 var readline = require('readline');
 var net = require('net');
 var fs = require("fs");
-var HOST = '127.0.0.1';
+var HOST = process.argv[2];
 var PORT = 6969;
 var counter = 0; // count the number of times server received a real data message
 var rec; // variable for get data
@@ -11,11 +11,14 @@ var ackCnt = 0;
 var dataName = "";
 var dataSize = 1;
 var receivedSize = 0;
-
+var dateStart = 0;
+var dateGetNow = 0;
+var datePass = 0;
 net.createServer(function(sock) {
   console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
     sock.on('data', function(data) {
       if(ackCnt == 0){
+        dateStart = Date.now();
         sock.write("ack1");
         dataName = data;
         out_filename = "./"+dataName;
@@ -40,14 +43,18 @@ net.createServer(function(sock) {
         rec=new Buffer(data);
         receivedSize = receivedSize + parseInt(data.length);
         counter++; 
-        readline.clearLine(process.stdout, 0);
-        readline.cursorTo(process.stdout, 0, null);
+        dateGetNow = Date.now();
+        var datePass=(dateGetNow-dateStart)/1000;
+        var speed=Math.floor(((receivedSize*8)/1000000)/datePass);
+        console.log('speed Mbits/sec : '+speed.toString()+" pass second = "+Math.floor(datePass).toString());
         console.log('data get :' + counter+' times. file get : '+Math.floor(receivedSize*100/dataSize).toString()+"%");
         fs.appendFileSync(out_filename, rec);
 
       }
 
       if(receivedSize == dataSize){
+        dateGetNow = Date.now();
+        var totalTime = Math.floor((dateGetNow-dateStart)/1000);
         sock.write('got it'+counter +"times");
         sock.write("finishack");
         dataSize = 1;
